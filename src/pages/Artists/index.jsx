9901a3de52664e4f4475f282d38ac2e1d8ad7460
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2023-12-21 00:01:45 -0300

    🎨 style(Artists/index.jsx): remove console.log statement 🔥 chore(Artists/index.jsx): remove unused import of Skeleton component 🚀 feat(Artists/index.jsx): add title attribute to artist images for better accessibility 🔧 chore(Artists/index.jsx): improve layout and styling of artist list items

--- src/pages/Artists/index.jsx
+++ src/pages/Artists/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { Header } from "../../components/Header";
 import { fetchUserTopItems } from "../../services/fetchUserTopItems";
+import { Skeleton } from "../Skeleton";
 
 export function Artists() {
   const [artists, setArtists] = useState([]);
@@ -14,8 +15,6 @@ export function Artists() {
     });
   }, []);
 
-  console.log(artists);
-
   const handleClickRedirectToArtist = (id) => {
     window.open(artists[id].external_urls.spotify);
   };
@@ -23,30 +22,31 @@ export function Artists() {
   return (
     <>
       <Header />
-      <section className="m-auto">
+      <section className="m-auto mb-8 flex max-w-[1200px] flex-col">
         <h1 className="py-12 text-center font-serif text-4xl">Artists</h1>
         {isLoading ? (
-          <p>Loading...</p>
+          <Skeleton />
         ) : (
-          <ul className="mx-20 my-8 grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-8">
+          <ul className="flex flex-wrap items-start justify-center gap-8 xl:justify-between">
             {artists.map(({ id, images, name, genres }, index) => {
               return (
-                <li key={id}>
-                  <div className="h-[200px] w-[200px] overflow-hidden rounded-lg">
+                <li key={id} className="flex w-[200px] flex-col">
+                  <div className="flex w-[200px] justify-center overflow-hidden rounded-lg shadow-md">
                     <img
-                      className="h-[200px] w-[200px] cursor-pointer object-cover shadow-md transition-all duration-200 ease-in-out hover:scale-110"
+                      className="block max-h-[200px] max-w-full cursor-pointer object-cover transition-all duration-200 ease-in-out hover:scale-110"
                       src={images[0].url}
                       alt={name}
+                      title={name}
                       onClick={() => {
                         handleClickRedirectToArtist(index);
                       }}
                     />
                   </div>
-                  <div className="mt-2 flex flex-col justify-start px-1">
-                    <div className="flex items-center gap-2">
+                  <div className="mt-2 flex flex-col justify-start">
+                    <div className="mt-1 flex items-center gap-2">
                       <p className="text-[0.9rem]">{index + 1}.</p>
                       <p
-                        className="cursor-pointer text-[0.9rem] font-semibold hover:underline"
+                        className="cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap text-[0.9rem] font-semibold hover:underline"
                         onClick={() => {
                           handleClickRedirectToArtist(index);
                         }}
