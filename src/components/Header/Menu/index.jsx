5b21b5eff976ac2555f29850b6d09983c799fbc8
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2023-12-21 00:57:46 -0300

    🔀 refactor(Menu): use useLocation hook to dynamically highlight active menu item 🎨 style(Menu): add conditional styling to highlight active menu item based on current pathname

--- src/components/Header/Menu/index.jsx
+++ src/components/Header/Menu/index.jsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function Menu() {
+  const { pathname } = useLocation();
+
   return (
     <nav className="m-auto border-b-[1px] border-zinc-500 bg-zinc-800 py-5">
       <ul className="flex justify-center gap-8 text-gray-100">
-        <Link to="/artists" className="font-medium hover:underline">
+        <Link
+          to="/artists"
+          className={`menu-item font-medium hover:text-indigo-400 hover:underline ${
+            pathname === "/artists" ? "text-indigo-400 underline" : ""
+          }`}
+        >
           Top Artists
         </Link>
-        <Link to="/genres" className="font-medium hover:underline">
+        <Link
+          to="/genres"
+          className={`menu-item font-medium hover:text-indigo-400 hover:underline ${
+            pathname === "/genres" ? "text-indigo-400 underline" : ""
+          }`}
+        >
           Top Genres
         </Link>
-        <Link to="/tracks" className="font-medium hover:underline">
+        <Link
+          to="/tracks"
+          className={`menu-item font-medium hover:text-indigo-400 hover:underline ${
+            pathname === "/tracks" ? "text-indigo-400 underline" : ""
+          }`}
+        >
           Top Tracks
         </Link>
       </ul>
