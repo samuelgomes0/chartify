a11bcc08885d0797c534fc26fa77879946afa2c0
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2023-12-21 00:02:54 -0300

    🎨 style(Skeleton/index.jsx): improve readability and structure of Skeleton component

--- src/pages/Skeleton/index.jsx
+++ src/pages/Skeleton/index.jsx
@@ -1,3 +1,15 @@
 export function Skeleton() {
-  return <div></div>;
+  return (
+    <ul className="flex animate-pulse flex-wrap items-start justify-center gap-8 xl:justify-between">
+      {[...Array(50)].map((_, index) => (
+        <li key={index} className="flex flex-col">
+          <div className="h-[200px] w-[200px] rounded-lg bg-gray-200"></div>
+          <div className="mt-4 flex flex-col justify-start gap-2">
+            <p className="h-[14px] w-[200px] rounded-md bg-gray-200"></p>
+            <p className="h-[10px] w-[100px] rounded-md bg-gray-200"></p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
